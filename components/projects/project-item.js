0b2f932e83cba30ffdfc15d33bfe3bc3a1eec84c
lyn <dldudsh82@gmail.com>
2023-07-17 20:35:08 +0900

    [update]사이트 바로가기 없을 때 없애기

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -13,8 +13,8 @@ function ProjectItem({ data }) {
     const calculatedPeriod = (start, end) => {
         const startDateStringArray = (start||'').split('-');
         const endDateStringArray = (end||'').split('-');
-var startDate = new Date(startDateStringArray[0], startDateStringArray[1] - 1, startDateStringArray[2]);
-var endDate = new Date(endDateStringArray[0], endDateStringArray[1] - 1, endDateStringArray[2]);
+        var startDate = new Date(startDateStringArray[0], startDateStringArray[1] - 1, startDateStringArray[2]);
+        var endDate = new Date(endDateStringArray[0], endDateStringArray[1] - 1, endDateStringArray[2]);
         const diffInMs = Math.abs(endDate - startDate);
         const result = diffInMs / (1000 * 60 * 60 * 24);
 
@@ -43,11 +43,16 @@ var endDate = new Date(endDateStringArray[0], endDateStringArray[1] - 1, endDate
             <div className="flex flex-col p-4">
                 <h1 className="text-2xl font-bold">{title}</h1>
                 <h3 className="mt-4 text-xl">{description}</h3>
-                <a href={githubLink}>깃허브 바로가기</a><br />
-                <a href={siteLink}>사이트 바로가기</a>
+       
                 <h3 className="my-1">
                     작업기간 : {startDate} ~ {endDate} ({calculatedPeriod(startDate,endDate)}일)
                 </h3>
+                <a href={githubLink}>깃허브 바로가기</a>
+                {
+                    siteLink && (
+                <a href={siteLink}>사이트 바로가기</a>
+                    )
+                }
                 <div className="flex flex-wrap items-start mt-2">
                     {tags.map((aTag) => (
                         <h1 className="px-2 py-1 mb-2 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" key={aTag.id}>{aTag.name}</h1>
